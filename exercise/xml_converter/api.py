da4bfe43917fbc9e4fc3795b81efe8905dd910a6
Luis Moreno <l.david1929@gmail.com>
2022-08-12 16:43:47 -0500

    Add files via upload

--- exercise/xml_converter/api.py
+++ exercise/xml_converter/api.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import action
+from rest_framework.exceptions import ParseError
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from xml_converter.parsers import XMLApolloParser
 
 
 class ConverterViewSet(ViewSet):
@@ -11,4 +13,9 @@ class ConverterViewSet(ViewSet):
 
     @action(methods=["POST"], detail=False, url_path="convert")
     def convert(self, request, **kwargs):
-        return Response({})
+        xml_file = request.FILES['file']
+        try:
+            converted = XMLApolloParser().parse(xml_file)
+        except ParseError:
+            raise ParseError
+        return Response(converted)
--- exercise/xml_converter/views.py
+++ exercise/xml_converter/views.py
@@ -1,10 +1,17 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render
+from rest_framework.exceptions import ParseError
+from xml_converter.forms import UploadFileForm
+from xml_converter.parsers import XMLApolloParser
 
 
 def upload_page(request):
     if request.method == 'POST':
-        # TODO: Convert the submitted XML file into a JSON object and return to the user.
-        return JsonResponse({})
-
-    return render(request, "upload_page.html")
+        xml_file = request.FILES['file']
+        try:
+            converted = XMLApolloParser().parse(xml_file)
+        except ParseError:
+            return HttpResponseBadRequest()
+        return JsonResponse(converted)
+    form = UploadFileForm()
+    return render(request, "upload_page.html", {"form": form})
